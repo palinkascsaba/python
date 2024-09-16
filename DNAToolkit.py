cf6e4bd5dd1df244b30ff32ca609f8bee1cb0ab0
palinkascsaba <palinkas.csaba22@gmail.com>
2024-09-16 22:39:40 +0200

    Add files via upload

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -8,7 +8,6 @@ def sequence_randomizer(length):
         random_sequence += random.choice(Nucleotides)
     return random_sequence    
 
-
 #DNS-szekvenciát validálja, hogy csak helyes nukleotidok vannak-e benne.
 def validateSeq(dna_seq):
     upperseq = dna_seq.upper()
@@ -23,3 +22,53 @@ def Nucount(seq):
     for nucleotide in seq:
         freqDict[nucleotide] += 1
     return freqDict
+
+def komplementator(sequence):
+    forditatlan_komplement = ''
+    komplement = ''
+    for nucleotide in sequence: 
+            if nucleotide == "A":
+                komplement_bazis = "T"
+            elif nucleotide == "T":
+                komplement_bazis = "A"
+            elif nucleotide == "G":
+                komplement_bazis = "C"
+            elif nucleotide == "C":
+                komplement_bazis = "G"       
+            forditatlan_komplement += komplement_bazis
+    komplement = forditatlan_komplement[::-1]
+    print (komplement)
+    return komplement
+
+#Gyorsabb, nincs annyi if statement (python specifikus de konnyebb)
+def gyorsabb_komplementator(seq):
+    mapping = str.maketrans('ATCG', 'TAGC')
+    return seq.translate(mapping)[::-1]
+
+#GC arányt mér
+def gc_content(seq):
+#Ha nincs semmi benne akkor 0-t ad ki error helyett.
+    if len(seq) == 0:
+        return 0.0
+    return round((seq.count('C') + seq.count('G')) / len(seq)*100,2)
+
+#fájlt elolvassa, sorokként listázza
+def readFile(filePath):
+    with open(filePath, 'r') as f:
+        return[l.strip() for l in f.readlines()]  
+    
+#fájlt elolvassa, sorokként listázza, dictionaryba-teszi key:label value:string,
+def FASTAread(filePath):
+    FASTAfile = readFile(filePath)
+#egy dictionary a DNS stringnek és a labelnek, ebbe már egybe vannak a sorok
+    FASTAdict = {}
+#A pillanatnyi label-t ez tartalmazza
+    FASTAlabel = ""
+#Rendezi a FASTAdict-be az adatot, egysorossá teszi
+    for line in FASTAfile:
+        if ">" in line:
+            FASTAlabel = line
+            FASTAdict[FASTAlabel] = ""
+        else:
+            FASTAdict[FASTAlabel] += line
+    return FASTAdict
